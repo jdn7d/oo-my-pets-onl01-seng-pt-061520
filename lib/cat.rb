a953ffc767587792100ea41fc8a893877cfd2d5e
Jessie Donovan <donovanajessie@gmail.com>
2020-07-20 00:59:31 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,11 +2,8 @@ class Cat
   # code goes here
   attr_accessor :name
   attr_writer :owner
-  def initialize(name)
+  def initialize(name, owner)
     @name = name
-
-  end
-  def owner=(owner)
    @owner = owner
 
  end
